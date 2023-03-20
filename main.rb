957be1a2d83472a46f6e89a656e83b082dda5a57
elarhadu <elarhadu@gmail.com>
2023-03-20 16:05:54 +0000

    Create a main.rb file that serves as the console app entry-point.

---
+++ main.rb
@@ -0,0 +1,77 @@
+require './app'
+
+def main
+  puts 'This a catalog of my things'
+  puts
+
+  loop do
+    case menu
+    when 1
+      puts 'List all books'
+      puts
+    when 2
+      puts 'List all music albums'
+      puts
+    when 3
+      puts 'List all movies'
+      puts
+    when 4
+      puts 'List of games'
+      puts
+    when 5
+      puts 'List all genres (e.g \'Comedy\', \'Thriller\')'
+      puts
+    when 6
+      puts 'List all labels (e.g. \'Gift\', \'New\')'
+      puts
+    when 7
+      puts 'List all authors (e.g. \'Stephen King\')'
+      puts
+    when 8
+      puts 'List all sources (e.g. \'From a friend\', \'Online shop\')'
+      puts
+    when 9
+      puts 'Add a book'
+      puts
+    when 10
+      puts 'Add a music album'
+      puts
+    when 11
+      puts 'Add a movie'
+      puts
+    when 12
+      puts 'Add a game'
+      puts
+    when 13
+      puts 'Thank you for using this App👋❌'
+      exit
+    else
+      puts 'Please select an option between 1 and 13'
+    end
+  end
+end
+
+def menu
+  puts 'Select a number'
+  list = {
+    1 => 'List all books',
+    2 => 'List all music albums',
+    3 => 'List all movies',
+    4 => 'List of games',
+    5 => 'List all genres (e.g \'Comedy\', \'Thriller\')',
+    6 => 'List all labels (e.g. \'Gift\', \'New\')',
+    7 => 'List all authors (e.g. \'Stephen King\')',
+    8 => 'List all sources (e.g. \'From a friend\', \'Online shop\')',
+    9 => 'Add a book',
+    10 => 'Add a music album',
+    11 => 'Add a movie',
+    12 => 'Add a game',
+    13 => 'Exit App'
+  }
+  list.each do |index, string|
+    puts "#{index} - #{string}"
+  end
+  gets.chomp.to_i
+end
+
+main
