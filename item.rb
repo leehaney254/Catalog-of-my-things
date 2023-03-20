5ee5b55464dc18d77a26564fd15f5d18b8b7a180
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-20 17:57:38 +0300

    Feat: Add group methods

--- item.rb
+++ item.rb
@@ -7,17 +7,20 @@ class Item
     @label = label
     @publish_date = publish_date
     @archived = archived
-
-    private
     @id = rand(1000)
     @archived = archived
   end
 
   def move_to_archive
+    @archived = true if can_be_archived? == true
   end
 
   private
   def can_be_archived?
+    return true if @publish_date > 10
+    false
   end
+
+  attr_wrtiter :id, :archived
 
 end
