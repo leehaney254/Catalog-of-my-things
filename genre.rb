6b68a76c41ab85b07bafb31b4490de06a278ef89
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-21 15:46:15 +0300

    Feat: Add self to genre

--- genre.rb
+++ genre.rb
@@ -1,12 +1,12 @@
 class Genre
- def initialize(name)
-  @id = Random.rand(1..1000)
-  @name = name
-  @items = []
- end
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
 
- def add_item(item)
-  @items << item
-  #item.set_genre(self)
- end
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
 end
