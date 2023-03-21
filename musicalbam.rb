60e2d04574cfb2d2e98638cfcd2f7d3eb8298622
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-21 12:35:21 +0300

    Feat: Add can_be_achieved method

--- musicalbam.rb
+++ musicalbam.rb
@@ -1,5 +1,12 @@
-class MusicAlbum
+require 'item.rb'
+
+class MusicAlbum < Item
   def initialize(on_spotify: false)
     @on_spotify = on_spotify
   end
+
+  def can_be_archived?
+    true if super && @on_spotify 
+
+    false
 end
