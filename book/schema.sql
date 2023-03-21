0363bdd89fbe91678a63785514b4c49353cdce4b
elarhadu <elarhadu@gmail.com>
2023-03-21 14:32:13 +0000

    Create table for labels and books

---
+++ book/schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE my_catalog
+
+-- books table
+CREATE TABLE books (
+id UUID PRIMARY KEY,
+publisher VARCHAR(100),
+cover_state VARCHAR(100),
+publish_date DATE,
+label_id UUID,
+CONSTRAINT fk_label FOREIGN KEY(label_id) REFERENCES labels(id)
+);
+
+-- labels table
+CREATE TABLE labels (
+  id UUID PRIMARY KEY,
+  title VARCHAR(100),
+  color VARCHAR(100)
+);
