d9b8399ad4a8a17ca93a1566b2ee7b41338b28d3
elarhadu <elarhadu@gmail.com>
2023-03-21 10:15:20 +0000

    Defined all Label class properties visible in the diagram and set up in the constructor method.

--- book/label.rb
+++ book/label.rb
@@ -1,5 +1,11 @@
-class Label<Item
-  def initialize
-    
+class Label
+  attr_accessor :title, :color
+  attr_reader :items, :id
+
+  def initialize(id, title, color)
+    @id = id
+    @title = title
+    @color = color
+    @items = []
   end
 end
