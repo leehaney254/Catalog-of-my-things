d0df13e893202c0b13e19dde70a338e8aa0c0218
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-21 18:12:42 +0300

    Feat: Add add_music_album method

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
+require './musicalbam'
+
 class App
-  def test_method
-    puts 'this is a test'
+  def initialize
+    @music_album = []
+  end
+
+  def add_music_album
+    print 'What day was it published(ie 10): '
+    date = gets.chomp.to_i
+    print 'Is it archived true or true or false: '
+    archived = gets.chomp
+    print 'Is it on spotify true or false: '
+    on_spotify = gets.chomp
+    puts 'Album Created succesfully.'
+    puts
+    music_album = MusicAlbum.new(date, archived: archived, on_spotify: on_spotify)
+    @music_album << music_album
   end
 end
