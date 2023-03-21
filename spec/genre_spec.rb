6b5e8c05b6d57fef916205896295c3003671aaf1
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-21 16:57:33 +0300

    Feat: Test genre

---
+++ spec/genre_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Genre do
+  before :each do
+    @genre = Genre.new('Action')
+  end
+
+  context 'When creatinga music album' do
+    it 'Should create a new object' do
+      expect(@genre).to be_an_instance_of Genre
+    end
+
+    it 'Should return the name action' do 
+      expect(@genre.name).to eq 'Action'
+    end
+
+    it 'Should add an item' do
+      item = Item.new(20, false)
+      expect(@genre.add_item(item).length).to eq 1
+    end
+  end
+end
