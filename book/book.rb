4e537f1ec96f2ab5088c30112908328eb5ac812f
elarhadu <elarhadu@gmail.com>
2023-03-21 10:05:31 +0000

    Define all Book class properties visible in the diagram and set up in the constructor method.

--- book/book.rb
+++ book/book.rb
@@ -1,3 +1,10 @@
-class Book<Item
-  
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state, publish_date, id)
+    super(publish_date, id)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 end
+ 
