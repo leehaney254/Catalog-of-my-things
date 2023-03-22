850f3d1b2a3d5cd416866baf613491631f1e9d58
elarhadu <elarhadu@gmail.com>
2023-03-22 23:39:13 +0000

    remove all files

--- sideClass/checkfile.rb
+++
@@ -1,7 +0,0 @@
-class CheckFile
-  def check_file(file, mode)
-    return File.open(file, mode) if File.exist?(file)
-
-    File.new(file, mode)
-  end
-end
--- spec/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-require_relative '../genre'
-require_relative '../musicalbam'
-require_relative '../item'
