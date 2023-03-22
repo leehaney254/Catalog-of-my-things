17a0dc12cde345d403fdc6aa29fae7b3be200284
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-22 08:28:47 +0300

    Feat: Add file storage

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require './musicalbam'
-require './module'
+require './albummodule'
 require './genre'
+require './datamodule'
+require './checkfile'
+require 'json'
 
 class App
   include AlbumModule
+  include DataModule
   def initialize
     @music_album = []
     @genre = []
+  end
+
+  def exit_app
+    genre_hash
+    music_album_hash
   end
 end
--- item.rb
+++ item.rb
@@ -9,6 +9,7 @@ class Item
 
   def move_to_archive
     @archived = true if can_be_archived? == true
+    @archived
   end
 
   private
