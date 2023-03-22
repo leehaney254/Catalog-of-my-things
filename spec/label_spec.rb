9b426ced3e65576aea2245b71b8cd3ae4febc9a2
elarhadu <elarhadu@gmail.com>
2023-03-22 19:24:05 +0000

    Create unit tests for methods in the label class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -4,7 +4,7 @@ require_relative '../book/label'
 describe 'Label class' do
   before do
     @book = Book.new('Jane Doe', 'good', '2023-03-21', 1)
-    @label = Label.new(1, 'new', 'green')
+    @label = Label.new('new', 'green')
   end
 
   it 'Has an item' do
