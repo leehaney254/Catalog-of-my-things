4a42c4d6153356d7bad344f03efde51331d6430f
elarhadu <elarhadu@gmail.com>
2023-03-22 19:18:49 +0000

    Modify item.rb file

--- item.rb
+++ item.rb
@@ -1,23 +1,25 @@
 class Item
-  attr_accessor :genre, :author, :source, :label
+  attr_accessor :genre, :author, :source, :label, :publish_date
 
-  def initialize(publish_date, archived)
+  def initialize(publish_date)
     @publish_date = publish_date
     @archived = archived
-    @id = rand(1000)
+    @id = Random.rand(1..1000)
   end
 
   def move_to_archive
     @archived = true if can_be_archived? == true
+    @archived
   end
 
   private
 
   def can_be_archived?
-    return true if @publish_date > 10
+    return true if @publish_date.to_i > 10
 
     false
   end
 
-  attr_wrtiter :id, :archived
+
+  attr_reader :id, :archived
 end
