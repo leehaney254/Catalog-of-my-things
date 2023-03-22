fb477bc5e25125c21c6c63fe994bc800c1df66dd
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-22 09:31:25 +0300

    Feat: Add readfile to initialize

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require './genre'
 require './datamodule'
 require './checkfile'
 require 'json'
+require './readfile'
 
 class App
   include AlbumModule
@@ -16,5 +17,10 @@ class App
   def exit_app
     genre_hash
     music_album_hash
+  end
+
+  def fetch_data
+    read_genre
+    read_music_album
   end
 end
